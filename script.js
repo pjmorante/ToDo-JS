f7dfe6653c25985a67b56e956612dd378d9da379
Pedro Morante <pedro.morante@outlook.com>
2022-10-20 18:07:52 -0500

    add task script.js

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const btn = document.querySelector('[data-form-btn]');
 const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  const content = `<div>
+                <i class="far fa-check-square icon"></i>
+                <span class="task">${value}</span>
+                </div>
+                <i class="fas fa-trash-alt trashIcon"></i>`;
+  task.innerHTML = content;
+  list.appendChild(task);
 };
 
 btn.addEventListener('click', createTask);
